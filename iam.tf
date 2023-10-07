7c1aed19ef87eb98761a17493c19e7f6d11576bf
Sriteja Kattamuru <131655915+sriteja28@users.noreply.github.com>
2023-10-07 12:06:26 +0530

    Updating Terraform files for the project

--- iam.tf
+++ iam.tf
@@ -14,9 +14,13 @@ resource "aws_iam_policy" "policy" {
           "ssm:GetParameterHistory",
           "ssm:GetParametersByPath",
           "ssm:GetParameters",
-          "ssm:GetParameter"
+          "ssm:GetParameter",
+          "kms:Decrypt"
         ],
-        "Resource" : "arn:aws:ssm:us-east-1:178724175855:parameter/roboshop.${var.env}.${var.component}.*"
+        "Resource" :[
+          "arn:aws:ssm:us-east-1:178724175855:parameter/roboshop.${var.env}.${var.component}.*",
+          var.kms_key_arn
+        ]
       }
     ]
   })
